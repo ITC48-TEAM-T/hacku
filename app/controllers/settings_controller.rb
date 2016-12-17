4de08bd32a3ef7cff848a768f4044591db36129f
t-kusakabe <ttmac0704@gmail.com>
2016-12-17 20:57:26 +0900

    display message

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,12 +1,11 @@
 class SettingsController < ApplicationController
   def index
-    @messages = YAML.load_file('config/message.yml')['nomal']
+    @message = YAML.load_file('config/message.yml')['friend']['nomal'][rand(5)]
   end
 
   def create
     saving = Saving.find_or_create_by(id: 1)
     saving.count += 1
     saving.save
-    redirect_to :root
   end
 end
