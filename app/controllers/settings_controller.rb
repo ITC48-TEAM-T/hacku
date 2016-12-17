f55cf6b4615a019574fbd1d362ad045494cfc100
t-kusakabe <ttmac0704@gmail.com>
2016-12-18 01:41:35 +0900

    system メッセージ表示

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,6 +1,8 @@
 class SettingsController < ApplicationController
   def index
     @message = YAML.load_file('config/message.yml')['friend']['nomal'][rand(5)].sub(/name/, 'foo')
+    @system_messages = Array.new
+    @system_messages << YAML.load_file('config/system_message.yml')['present']
     @user = User.find_or_create_by(id: 1)
     @worry_count = Worry.find_or_create_by(id: 1).count
   end
