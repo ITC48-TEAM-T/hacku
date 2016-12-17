01b08d76b745f67597a31ff4283c17b7272b810b
t-kusakabe <ttmac0704@gmail.com>
2016-12-18 01:56:55 +0900

    worry function

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -5,12 +5,17 @@ class SettingsController < ApplicationController
     @system_messages << YAML.load_file('config/system_message.yml')['present']
     @user = User.find_or_create_by(id: 1)
     @worry_count = Worry.find_or_create_by(id: 1).count
+    if Saving.find_or_create_by(id: 1).count < 10
+      @image = 
   end
 
   def create
     saving = Saving.find_or_create_by(id: 1)
     saving.count += 1
     saving.save
+    worry = Worry.find_or_create_by(id: 1)
+    worry.count -= 1
+    worry.save
     redirect_to action: :index
   end
 end
