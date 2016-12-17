039e59afc787e98a8563f8fcddc877c19016898a
t-kusakabe <ttmac0704@gmail.com>
2016-12-18 00:17:28 +0900

    名前の正規表現

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,6 +1,7 @@
 class SettingsController < ApplicationController
   def index
-    @message = YAML.load_file('config/message.yml')['friend']['nomal'][rand(5)]
+    @message = YAML.load_file('config/message.yml')['friend']['nomal'][rand(5)].sub(/name/, 'foo')
+    @user = User.find_or_create_by(id: 1)
   end
 
   def create
