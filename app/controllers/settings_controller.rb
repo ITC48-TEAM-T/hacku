8da8a6048ddbf534d6fab05cfb308ae230b8c6e3
t-kusakabe <ttmac0704@gmail.com>
2016-12-18 09:26:51 +0900

    add present

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,8 +1,9 @@
 class SettingsController < ApplicationController
   def index
     @message = YAML.load_file('config/message.yml')['friend']['nomal'][rand(5)].sub(/name/, 'foo')
+    system_messages = YAML.load_file('config/system_message.yml')
     @system_messages = Array.new
-    @system_messages << YAML.load_file('config/system_message.yml')['present']
+    @system_messages << system_messages['treat']
     @user = User.find_or_create_by(id: 1)
     @worry_count = Worry.find_or_create_by(id: 1).count
     @saving =  Saving.find_or_create_by(id: 1).count
@@ -10,6 +11,43 @@ class SettingsController < ApplicationController
       @image = 'everyday.png'
     else
       @image = 'deat.png'
+      if Saving.find_or_create_by(id: 1).count == 10
+        @system_messages << system_messages['girlfriend']
+      elsif Saving.find_or_create_by(id: 1).count == 20
+        @system_messages << system_messages['bracelet']
+      elsif Saving.find_or_create_by(id: 1).count == 30
+        @system_messages << system_messages['bracelet']
+      elsif Saving.find_or_create_by(id: 1).count == 40
+        @system_messages << system_messages['earring']
+      elsif Saving.find_or_create_by(id: 1).count == 50
+        @system_messages << system_messages['earring']
+      elsif Saving.find_or_create_by(id: 1).count == 60
+        @system_messages << system_messages['earring']
+      elsif Saving.find_or_create_by(id: 1).count == 70
+        @system_messages << system_messages['hairclip']
+      elsif Saving.find_or_create_by(id: 1).count == 80
+        @system_messages << system_messages['hairclip']
+      end
+    end
+
+    if Saving.find_or_create_by(id: 1).count >= 70
+      @hairclip = 'hairclip_1.png'
+    elsif
+      @hairclip = 'hairclip_2.png'
+    end
+
+    if Saving.find_or_create_by(id: 1).count >= 60
+      @earring = 'earring_3.png'
+    elsif Saving.find_or_create_by(id: 1).count >= 50
+      @earring = 'earring_2.png'
+    elsif Saving.find_or_create_by(id: 1).count >= 40
+      @earring = 'earring_1.png'
+    end
+
+    if Saving.find_or_create_by(id: 1).count >= 30
+      @bracelet = 'bracelet_2.png'
+    elsif Saving.find_or_create_by(id: 1).count >= 20
+      @bracelet = 'bracelet_1.png'
     end
 
   end
